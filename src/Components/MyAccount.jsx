03b86c3a344bae16b40dad1c74fc81386ff20d15
Rob Witkowski <rob_witkowski@hotmail.com>
2024-03-19 16:58:00 -0400

    rendered submitted new appointments on confirmation page

--- src/Components/MyAccount.jsx
+++ src/Components/MyAccount.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
+import { getAppointments } from "../Services/ScheduledApptService.jsx";
 import"./MyAccount.css"
+import { getUserByEmail } from "../Services/UserService.jsx";
 
 export const MyAccount = () => {
+const [appointments, setAppointments] = useState([])
+
+
+
+useEffect(() => {
+    getAppointments().then((allAppts) => {
+          setAppointments(allAppts);
+        });
+      }, []);
+
+
+
     return (
         <div className="myaccount-container">
             <h1>
--- src/Components/ScheduledAppt.jsx
+++ src/Components/ScheduledAppt.jsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
+import { getAppointments } from "../Services/ScheduledApptService.jsx";
+import"./MyAccount.css"
+import { getUserByEmail } from "../Services/UserService.jsx";
+import { useParams } from "react-router-dom";
+
 export const MyScheduledAppt = () => {
+const [appointments, setAppointments] = useState([])
+const [loggedUser, setLoggedUser] = useState([])
+
+const {appointmentId} = useParams()
+
+
+useEffect(() => {
+    getAppointments(appointmentId).then((data) => {
+            const appointmentObj = data
+          setAppointments(appointmentObj);
+        });
+      }, [appointmentId]);
+
+useEffect(() => {
+    getUserByEmail().then((loggedUser) => {
+          setLoggedUser(loggedUser);
+        });
+      }, []);
+
+
     return (
         <div className="myschedule-container">
             <h1>
-
                 <span>Newark Barber Shop</span>
             </h1>
+
             <h2>You're all set!</h2>
         <div>
-            You're scheduled with {}
+           
+           <div className="all-appointments">{appointments.map(appointment => {
+            return (
+                <div key={appointment.id} >You're scheduled with: {appointment.barber} on {appointment.month}{appointment.day} at {appointment.time}</div>
+            )
+           })}</div>
+          
         </div>
 
         </div>
